cf0a2c614033074f507f79f6e63bd380c714936d
alid0217354 <44001483+alid0217354@users.noreply.github.com>
2018-10-31 23:32:59 +0800

    Create MainQueueString.java

---
+++ MainQueueString.java
@@ -0,0 +1,14 @@
+package ASD3;
+import sun.misc.Queue;
+public class MainQueueString {
+
+	public static void main(String[] args) {
+		QueueString q = new QueueString();
+		q.cetak();
+		
+		q.insert("ALI");q.cetak();
+		q.insert("BABA");q.cetak();
+	
+
+	}
+}
