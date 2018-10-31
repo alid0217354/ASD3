c4d2ab5f049810e0e0ac49c813b1a912d873c9f3
alid0217354 <44001483+alid0217354@users.noreply.github.com>
2018-10-31 23:33:42 +0800

    Create MainStackString .java

---
+++ MainStackString .java
@@ -0,0 +1,15 @@
+package ASD3;
+
+public class MainStackString {
+public static void main(String[] args) {
+		classstackstring st= new classstackstring();
+		
+		st.push("ali");
+    st.push("baba");
+	
+		
+		String d0=st.pop();
+		System.out.println(d0);
+		st.cetak();
+	}
+}
